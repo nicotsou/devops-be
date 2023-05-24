ae637f7e0af2dba69e8cf15742d4b9c0b296c97b
Nick Tsourektsidis <nick_tsourektsidis@epam.com>
2023-05-24 09:23:00 +0200

    Fix linter problems

--- src/handlers/carts/pipes/transform.pipe.ts
+++ src/handlers/carts/pipes/transform.pipe.ts
@@ -1,14 +1,12 @@
-import { PipeTransform, Injectable, ArgumentMetadata } from '@nestjs/common';
+import { PipeTransform, Injectable } from '@nestjs/common';
 import { CreateCartDto, CreateCartDtoValid } from '../dto/create-cart.dto';
 
 @Injectable()
 export class TransformPipe implements PipeTransform {
-  constructor() {}
-
-  transform(value: CreateCartDto, metadata: ArgumentMetadata): CreateCartDtoValid {
+  transform(value: CreateCartDto): CreateCartDtoValid {
     return {
       balance: +value.balance || 0,
       currency: value.currency.toUpperCase(),
-    }
+    };
   }
 }
